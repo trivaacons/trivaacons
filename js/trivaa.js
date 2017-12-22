76c601575b78e1911d85c38a49ca105477cf6b8b
Krisztian Balazs Szaniszlo <krisztian.szaniszlo@trivaalogic.hu>
2017-12-22 14:42:42 +0100

    * Added language selectors. * Made some responsive layout changes on activities part. * Attempted to reduce FOUC. The image loads are still going to be flashing a bit...

--- js/trivaa.js
+++ js/trivaa.js
@@ -1,116 +1,118 @@
-/**
- * Mobile Browser Address-bar Resize Jump Fix.
- *
- * Prevent items with a height depending on the viewport from jumping if the
- * mobile browser address bar appreas/disappears.
- * This fixes the address-bar viewport resize jump problem of many mobile
- * Browsers.
- *
- * Usage:
- *
- *   Put this data attribute: "data-jump-fix" on each html element that has
- *   height depending on the vp height.
- *
- *   load the script and call JumpFix.init()
- *
- */
-var JumpFix = {
-  fixItems: $('*[data-jump-fix]'), // Get all items with the data attribute.
-  isScrolling: false,
-  timeoutScroll: null,
+$(function () {
+  /**
+   * Mobile Browser Address-bar Resize Jump Fix.
+   *
+   * Prevent items with a height depending on the viewport from jumping if the
+   * mobile browser address bar appreas/disappears.
+   * This fixes the address-bar viewport resize jump problem of many mobile
+   * Browsers.
+   *
+   * Usage:
+   *
+   *   Put this data attribute: "data-jump-fix" on each html element that has
+   *   height depending on the vp height.
+   *
+   *   load the script and call JumpFix.init()
+   *
+   */
+  const JumpFix = {
+    fixItems: $('*[data-jump-fix]'), // Get all items with the data attribute.
+    isScrolling: false,
+    timeoutScroll: null,
 
-  // This sets 'isScrolling' to true for a certain amount of time.
-  setScrollingStatus: function () {
-    if (JumpFix.timeoutScroll) {
-      clearTimeout(JumpFix.timeoutScroll);
-    }
-    JumpFix.isScrolling = true;
-    JumpFix.timeoutScroll = setTimeout(function () {
-      JumpFix.isScrolling = false;
-    }, 100);
-  },
-
-  // Save actual heights
-  saveHeights: function () {
-    JumpFix.fixItems.each(function (i) {
-      JumpFix.fixItems[i].savedHeight = $(this).height();
-    })
-  },
-
-  // window and document actions here
-  bindUIActions: function () {
-
-    // Set isScrolling when the user scrolls or swipes.
-    $(window).scroll(function () {
-      if (JumpFix.isScrolling === false) {
-        JumpFix.saveHeights();
+    // This sets 'isScrolling' to true for a certain amount of time.
+    setScrollingStatus: function () {
+      if (JumpFix.timeoutScroll) {
+        clearTimeout(JumpFix.timeoutScroll);
       }
-      JumpFix.setScrollingStatus();
-    });
+      JumpFix.isScrolling = true;
+      JumpFix.timeoutScroll = setTimeout(function () {
+        JumpFix.isScrolling = false;
+      }, 100);
+    },
 
-    $(window).on({
-      'touchmove': function () {
+    // Save actual heights
+    saveHeights: function () {
+      JumpFix.fixItems.each(function (i) {
+        JumpFix.fixItems[i].savedHeight = $(this).height();
+      })
+    },
+
+    // window and document actions here
+    bindUIActions: function () {
+
+      // Set isScrolling when the user scrolls or swipes.
+      $(window).scroll(function () {
         if (JumpFix.isScrolling === false) {
           JumpFix.saveHeights();
         }
         JumpFix.setScrollingStatus();
-      }
-    });
+      });
 
-    // On resize: if user is scrolling use the saved height.
-    // if user is NOT scrolling save the new nativ height after resize.
-    $(window).resize(function () {
-      if (JumpFix.isScrolling === true) {
-        JumpFix.fixItems.each(function (i) {
-          $(this).height(JumpFix.fixItems[i].savedHeight);
-        })
-      } else {
-        JumpFix.fixItems.each(function () {
-          $(this).css('height', '');
-        });
-      }
-    });
-  },
+      $(window).on({
+        'touchmove': function () {
+          if (JumpFix.isScrolling === false) {
+            JumpFix.saveHeights();
+          }
+          JumpFix.setScrollingStatus();
+        }
+      });
 
-  init: function () {
-    this.bindUIActions();
-  }
+      // On resize: if user is scrolling use the saved height.
+      // if user is NOT scrolling save the new nativ height after resize.
+      $(window).resize(function () {
+        if (JumpFix.isScrolling === true) {
+          JumpFix.fixItems.each(function (i) {
+            $(this).height(JumpFix.fixItems[i].savedHeight);
+          })
+        } else {
+          JumpFix.fixItems.each(function () {
+            $(this).css('height', '');
+          });
+        }
+      });
+    },
 
-};
-
-/**
- * Configures smooth scroll of inner links within the specified container. Makes sure the location is not updated with the
- * hashtag of the anchor.
- * 
- * @param container
- *            JQuery object pointing to the container(s) in which to look for anchors
- */
-function configureScrollToTarget(container) {
-  container.find('a[href*="#"]:not([href="#"])').off('click').click(function (e) {
-    // This prevents the update of the location bar.
-    e.preventDefault();
-
-    // Find the target anchor.
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-
-    // Scroll to it.
-    if (target.length) {
-      var scrollTargetOffset = target.offset().top - $('.navbar').outerHeight();
-      $('html, body').animate({
-        scrollTop: scrollTargetOffset
-      }, 400);
+    init: function () {
+      this.bindUIActions();
     }
-  });
-}
+  };
 
-/**
- * Page initialization.
- */
-$(function () {
+  /**
+   * Configures smooth scroll of inner links within the specified container. Makes sure the location is not updated with the
+   * hashtag of the anchor.
+   * 
+   * @param container
+   *            JQuery object pointing to the container(s) in which to look for anchors
+   */
+  function configureScrollToTarget(container) {
+    container.find('a[href*="#"]:not([href="#"])').off('click').click(function (e) {
+      // This prevents the update of the location bar.
+      e.preventDefault();
+
+      // Find the target anchor.
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+      // Scroll to it.
+      if (target.length) {
+        var scrollTargetOffset = target.offset().top - $('.navbar').outerHeight();
+        $('html, body').animate({
+          scrollTop: scrollTargetOffset
+        }, 400);
+      }
+    });
+  }
+  /**
+   * Page initialization.
+   */
+
   // Initialize the mobile browser 'address bar jump' fix.
   JumpFix.init();
 
   // Configure smooth scrolling.
   configureScrollToTarget($('html, body'));
+
+  // Show the page.
+  $('.no-fouc').removeClass('no-fouc');
 });
